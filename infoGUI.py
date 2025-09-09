f9ec598e5822a2fc51d56434e443dcedc8d71fe8
Kimmo <kimmo.ripatti@gmail.com>
2025-09-09 10:45:03 +0300

    Delete files

--- infoGUI.py
+++ infoGUI.py
@@ -9,10 +9,10 @@ def percent():
     label.config(text=str(cpupercent), font=("Freesans", 15))
     root.after(1000, percent) # Schedule this function to run again after 1000ms (or 1 second).
 
-def freq():
-    cpufrequenzy = psutil.cpu_freq()
-    label2.config(text=str(cpufrequenzy), font=("Freesans", 15))
-    root.after(1000, freq) # Schedule this function to run again after 1000ms (or 1 second).
+#def freq():
+#    cpufrequenzy = psutil.cpu_freq()
+#    label2.config(text=str(cpufrequenzy), font=("Freesans", 15))
+#    root.after(1000, freq) # Schedule this function to run again after 1000ms (or 1 second).
 
 root = tk.Tk()
 label = tk.Label(root)
@@ -21,11 +21,11 @@ cpu = tk.Label(root, text="CPU %", font=("Freesans", 20, "bold"))
 cpu.pack()
 label.pack()
 
-cpufreq = tk.Label(root, text="CPU frequenzy", font=("Freesans", 20, "bold"))
-cpufreq.pack()
-label2.pack()
+#cpufreq = tk.Label(root, text="CPU frequenzy", font=("Freesans", 20, "bold"))
+#cpufreq.pack()
+#label2.pack()
 
 percent() # start the event loop with an initial call to update_label().
-freq()
+#freq()
 
 root.mainloop()
